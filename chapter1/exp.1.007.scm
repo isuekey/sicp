29f528d3cd672bc6e7ae67e5ef2d837b5c124c00
liuhanru <isuekey@163.com>
2021-01-25 17:37:43 +0800

    fix

--- chapter1/exp.1.007.scm
+++ chapter1/exp.1.007.scm
@@ -1,17 +1,42 @@
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x) x)))
-;(define (good-enough? res x)
-;  (= (improve res x) res))
 (define (improve guess x)
   (average guess (/ x guess)))
 (define (average x y)
   (/ (+ x y) 2))
 (define (square x) (* x x))
+
+
+(define (sqrt-iter guess x oldGuess)
+  (if (good-enough? guess x oldGuess)
+      guess
+      (sqrt-iter (improve guess x) x guess)))
+;(define (good-enough? res x)
+;  (= (improve res x) res))
 (define (sqrt x)
-  (if (= x 0.) 0.
-      (sqrt-iter 1.0 x)))
+  (sqrt-iter (improve 1. x) x 1.0))
+(define (good-enough? guess x oldGuess)
+  (<= (abs (- x (square guess)))
+      (abs (- x (square oldGuess)))
+      ))
+
+;; the best precision
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
 (define (good-enough? guess x)
-  (< (abs (- x (square guess)))
-     (* x 0.00001)))
+  (= (improve guess x) guess))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+;; (sqrt 0.) will Floating-point underflow
+
+
+;; 使用梯度检查
+(define (sqrt-iter guess x oldGuess)
+  (if (good-enough? guess x oldGuess)
+      guess
+      (sqrt-iter (improve guess x) x guess)))
+(define (good-enough? guess x oldGuess)
+  (<= (abs (- guess oldGuess))
+      (* guess 0.001)))
+(define (sqrt x)
+  (sqrt-iter 1.0 x 2.0))
