bd0186324dfda7fdb4e9caf2673e8b62d4dd67a5
liu.hanru <isuekey@github.com>
2021-03-04 22:35:08 +0800

    04 03 2021 home

---
+++ chapter2/exp.2.061.scm
@@ -0,0 +1,8 @@
+
+;; 生序表
+(define (adjoin-set x set)
+  (cond ((null? set) (cons x set))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
