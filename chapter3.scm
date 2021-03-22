8993329120e5aa0a8cfea2bb869ce842b8e8003f
liu.hanru <isuekey@github.com>
2021-03-22 22:23:47 +0800

    chapter 3

--- chapter3.scm
+++ chapter3.scm
@@ -147,3 +147,14 @@ E4
 |#
 
 ;; 内部定义
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
