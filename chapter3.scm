7dd2085b416ffca45d4bf9ada7640dbe6de0a283
liuhanru <isuekey@163.com>
2021-03-29 11:41:45 +0800

    29 03 2021

--- chapter3.scm
+++ chapter3.scm
@@ -221,6 +221,114 @@ z2
 (define (set-car! z new-value) ((z 'set-x!) new-value) z)
 (define (set-cdr! z new-value) ((z 'set-y!) new-value) z)
 
+#|
+简单的队列实现
+(make-queue)
+(empty-queue? que)
+(front-queue? que)
+(insert-queue! que itm)
+(delete-queue! que) 删除第一个并返回
+|#
 
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr queue item))
 
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "front called with an empty queue" queue)
+      (car queue)))
+(define (insert-queue? queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "delete! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
 
+#|
+  一维表格
+  带表头单元的表
+  (cdr table) 表示表的内容
+  (car table) 表的表头
+|#
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record (cdr record)
+        #f)))
+(define (assoc key records)
+  (cond ((null? records) #f)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+(define (insert! key val table)
+  (let ((record (assoc key (cdr table))))
+    (if record (set-cdr! record val)
+        (set-cdr! table
+                  (cons (cons key val) (cdr table)))))
+  'ok)
+
+#|
+  两维表格
+|#
+(define (lookup key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record (cdr record)
+              #f))
+        #f)))
+(define (insert! key-1 key-2 value table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record (set-cdr! record value)
+              (set-cdr! subtable (cons (cons key-2 value)
+                                       (cdr subtable)))))
+        (set-cdr! table
+                  (cons (list key-1 (cons key-2 value))
+                        (cdr table)))))
+  'ok)
+#|
+  创建局部的表格
+|#
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record (cdr record)
+                  #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record (set-cdr! record value)
+                  (set-cdr! subtable (cons (cons key-2 value)
+                                           (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc) insert!)
+            (else (error "unknown operation -- table" m))))
+    dispatch))
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc))
